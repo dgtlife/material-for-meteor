91d291f37bda94b6d3cff85fb8c532ea8a688b9e
Derek Gransaull <derek@dgtlife.com>
2017-04-19 14:33:31 -0700

    Uped the version number based on last change

--- package.js
+++ package.js
@@ -5,7 +5,7 @@
  */
 Package.describe({
   summary: 'A Blaze implementation of Material Design components for Meteor',
-  version: '0.7.1',
+  version: '0.7.2',
   name: 'dgtlife:material',
   git: 'https://github.com/dgtlife/material-for-meteor.git'
 });
